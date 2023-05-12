5ed8c399496f20f87074a36948ad56d8d4cd050f
TristanM225 <TristanM225@gmail.com>
2023-05-11 19:18:26 -0700

    pulled main

---
+++ Assets/JS/script.js
@@ -0,0 +1,6 @@
+var options = ['a', 'b', 'c', 'd']
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
